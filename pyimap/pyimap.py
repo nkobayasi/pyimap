e0d1976d4c69c50fb3f5c9dfcf27851bfc0ff728
nkobayasi <noriyuki.kobayasi@gmail.com>
2026-08-18 15:46:56 +0900

    annotationの追加

--- pyimap/pyimap.py
+++ pyimap/pyimap.py
@@ -4,14 +4,14 @@ import re
 import base64
 
 def encode_imap_utf7(s:str) ->bytes:
-    def repl(m):
+    def repl(m:re.Match) ->str:
         return '&' + base64.b64encode(m.group(0).encode('UTF-16BE'), altchars=b'+,').rstrip(b'=').decode('ascii') + '-'
     if not isinstance(s, str):
         raise TypeError('should be str')
     return re.sub(r'[^\u0020-\u007e]+', repl, s.replace('&', '&-')).encode('ascii')
 
 def decode_imap_utf7(b:bytes) ->str:
-    def repl(m):
+    def repl(m:re.Match) ->str:
         return base64.b64decode(m.group(1) + '='*(-len(m.group(1))%4), altchars='+,').decode('UTF-16BE')
     if not isinstance(b, bytes):
         raise TypeError('should be bytes')
